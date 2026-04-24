823e8cf2ad061fe95d265d17f3afb044e641ee66
afsanifti <afsanisifti@gmail.com>
2026-04-24 16:36:22 +0600

    Admin control from product system class. Yet to add boundaries to all the functions

--- main.cpp
+++ main.cpp
@@ -11,13 +11,6 @@
 
 using namespace std;
 
-/**
- * Things admin can do:
- *      1. Login with credential
- *      2. Add products in the list
- *      3. Update stocks
- *      4. Update prices
- */
 
 class Product {
 private:
@@ -52,23 +45,48 @@ public:
         // TODO: add limits for index
         products[index].setPrice(newPrice);
     }
+
+    static void updateStock(int index, int newStock) {
+        products[index].setStock(newStock);
+    } 
 };
+
+/**
+ * Things admin can do:
+ *      1. Login with credential
+ *      2. Add products in the list
+ *      3. Update stocks
+ *      4. Update prices
+ */
 
 class Admin {
 private:
     const string admin_username = "Afsan";
     const string admin_password = "2309012";
     bool isLoggedIn;
+
 public:
     Admin() : isLoggedIn(false) {}
 
     bool login(string username, string password) {
-        if (admin_username == username) {
+        if (admin_username == username && admin_password == password) {
             isLoggedIn = true;
             return true;
         } else {
             return false;
         }
+    }
+
+    void addProduct(string name, float price, int stock) {
+        ProductSystem::addProducts(name, price, stock);
+    }
+
+    void updatePrice(int index, float newPrice) {
+        ProductSystem::updatePrice(index, newPrice);
+    }
+
+    void updateStock(int index, int newStock) {
+        ProductSystem::updateStock(index, newStock);
     }
 };
 
